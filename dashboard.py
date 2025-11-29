a6f4b89015539c8964899a9f0aceda15884ab9db
Imran Tayyab <imrantayyabllc@gmail.com>
2025-11-28 21:17:44 -0800

    Initial commit - Dash dashboard project

---
+++ dashboard.py
@@ -0,0 +1,30 @@
+import dash
+from dash import dcc, html  # Updated imports
+
+# create a dash application 
+app = dash.Dash(__name__)
+
+# Define the layout of the dashboard 
+app.layout = html.Div(
+    children=[
+        html.H1('My Dashboard'),
+        dcc.Graph(
+            id='My-Graph',
+            figure={
+                'data': [   # 'Data' changed to 'data'
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Bar chart'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': 'line chart'},
+                ],
+                'layout': {
+                    'title': 'Graph title',
+                    'xaxis': {'title': 'x-axis'},
+                    'yaxis': {'title': 'y-axis'}
+                }
+            }
+        )
+    ]
+)
+
+# Run the application
+if __name__ == '__main__':
+    app.run_server(debug=True)
